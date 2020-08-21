4a6e4c24586ceb69fe6103e882aa323c27eed309
Thomas <thomas@craftacademy.se>
2020-08-21 11:25:21 +0200

    code cleaning

--- App.js
+++ App.js
@@ -1,8 +1,6 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import ArticlesIndex from './src/components/ArticlesIndex'
 import AboutUs from './src/components/AboutUs'
-import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
